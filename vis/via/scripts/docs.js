be99a0beb6bfa759972cd4e6c3258a0cf1bcb3d8
Yali Bian <Yali.Bian@hotmail.com>
2017-01-30 13:02:00 -0500

    Update my cv and via project

---
+++ vis/via/scripts/docs.js
@@ -0,0 +1,20 @@
+// fill the document-view using the source dataset.
+
+function docs() {
+
+    source.forEach(function (doc, index) {
+        // console.log(doc, index);
+
+        $("#document-view").append("<div class='doc panel panel-default' id=" + doc.docid + "></div>");
+        $("#" + doc.docid).append("<h3 class='panel-heading doc-title'>" + doc.docid + "</h3>")
+            .append("<div class='panel-body doc-content'>" + doc.doctext + "</div>");
+    });
+
+
+    // for search box
+
+
+}
+
+
+
